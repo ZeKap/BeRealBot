77ec514b8a62fe558e66bf5efd3a8ba456b2a187
ZeKap <kapismath@gmail.com>
2022-09-10 19:01:05 +0200

    finished work

---
+++ main.py
@@ -0,0 +1,70 @@
+import tweepy
+
+from tokens import apiKey, apiSecret, accessToken, accessSecret
+
+from random import randrange
+import time
+
+# vars for prog
+debug = True
+maxTweets = 5
+minTweets = 1
+moments = []
+
+# try to connect with tweepy api
+def connect_api():
+    auth = tweepy.OAuthHandler(apiKey, apiSecret)
+    auth.set_access_token(accessToken, accessSecret)
+
+    global api  # declare global var api to send tweet in others func
+    api = tweepy.API(auth)
+
+    try:    # try to connect and exit in case of error
+        api.verify_credentials()
+        print("Tweepy credentials are ok")
+    except:
+        print("Error in tweepy")
+
+# generate a random moment in day
+def random_moment() -> str:
+    hour = randrange(0, 23)     # random hour
+    minute = randrange(0, 59)   # random minute
+    if(hour) < 10: hour = '0' + str(hour)       # store in str and add '0' if < 10
+    else: hour = str(hour)
+    if(minute) < 10: minute = '0' + str(minute) # store in str and add '0' if < 10
+    else: minute = str(minute)
+    return hour + ":" + minute  # return in format '00:00'
+
+# generate random number of tweets in the day
+def nb_tweets() -> int:
+    return randrange(minTweets, maxTweets)
+
+# tweet a tweet
+def make_a_tweet():
+    from tweets import tweetsList                   # get list of tweets possible
+    tweet = tweetsList[randrange(len(tweetsList))]  # get random tweet from tweet list
+    if('lastTweet' in globals()):                   # if there is a last tweet available
+        while(tweet == lastTweet):                  # while the new tweet is the same as the old one
+            tweet = tweetsList[randrange(len(tweetsList))]  # take a tweet
+    lastTweet = tweet                               # store new tweet in lastTweet
+    if(debug): print("Tweet: " + tweet)
+    api.update_status(tweet)                        # tweet a tweet
+
+# generate a random number of jobs
+def create_moments():
+    if(debug): print("clearing jobs")
+    moments.clear()
+    for _ in range(nb_tweets()):
+        moments.append(random_moment())
+    print(moments)
+
+if(__name__ == "__main__"):
+    connect_api()                               # connect to tweepy API
+    create_moments()                            # create jobs
+    while True:
+        currentTime = time.strftime("%H:%M")    # get current time
+        if(currentTime in moments):             # if it's time to do a tweet
+            make_a_tweet()                      # do a tweet
+        if(currentTime == "00:00"):             # if it's midnight
+            create_moments()                    # delete and recreate random schedule jobs
+        time.sleep(50)                          # sleep for 50 seconds before retrying
---
+++ tweets.py
@@ -0,0 +1,8 @@
+tweetsList =[
+    "BeRealBot: ⏳ Time to post a photo of what you're doing!",
+    "BeRealBot: ⏰ Quick! Show us what you're doing!",
+    "BeRealBot: 😉 You know what time it is!",
+    "BeRealBot: 📷 You can tell what you're doing, but take a picture of it is better",
+    "BeRealBot: 😈 What do you mean it's not time to post? We don't care share it!",
+    "BeRealBot: 😱 You'll never guess what your friends are doing! (what clickbait?)"
+]
